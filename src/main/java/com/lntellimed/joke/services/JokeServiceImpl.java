be36d2373e0e2b623a84b5b7dbcc4f5c9e619700
“Sam <sam.alsmadi@gmail.com>
2018-10-09 22:17:09 -0400

    refact: use Java config

--- src/main/java/com/lntellimed/joke/services/JokeServiceImpl.java
+++ src/main/java/com/lntellimed/joke/services/JokeServiceImpl.java
@@ -6,9 +6,9 @@ import guru.springframework.norris.chuck.ChuckNorrisQuotes;
 @Service
 public class JokeServiceImpl implements JokeService {
 	private final ChuckNorrisQuotes chuckNorrisQuotes;
-	
-	public JokeServiceImpl() {
-		this.chuckNorrisQuotes = new ChuckNorrisQuotes();
+
+	public JokeServiceImpl(ChuckNorrisQuotes chuckNorrisQuotes) {
+		this.chuckNorrisQuotes = chuckNorrisQuotes;
 	}
 
 	@Override
